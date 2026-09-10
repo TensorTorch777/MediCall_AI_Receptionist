b1be654b120782c942dfcee6e506959a51e8125a
Nimish Vadgaonkar <116025943+TensorTorch777@users.noreply.github.com>
2026-09-10 23:03:49 +0530

    Delete fonoster directory

--- fonoster/.scripts/copy-prisma-clients.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Copying Prisma clients to dist folders"
-
-mkdir -p mods/identity/dist/generated/@prisma
-cp -r mods/identity/src/generated/@prisma/client mods/identity/dist/generated/@prisma
--- fonoster/mods/apiserver/migrations/20250105184832_make_credentials_optional/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "intelligence_services" ALTER COLUMN "credentials_hash" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "stt_services" ALTER COLUMN "credentials_hash" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "tts_services" ALTER COLUMN "credentials_hash" DROP NOT NULL;
